746f82dad91972014da8b6efddf6cd27cb4d5d3f
katehoang1989 <60130336+katehoang1989@users.noreply.github.com>
2020-03-08 10:26:44 -0500

    Update vst-install.sh

--- vst-install.sh
+++ vst-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 yum install curl wget -y
-curl -O https://raw.githubusercontent.com/phuongnamsoft/vestacp/master/vst-install-rhel.sh
-curl -O https://raw.githubusercontent.com/phuongnamsoft/vestacp/master/vst-crack.sh
+curl -O https://raw.githubusercontent.com/katehoang1989/centos_vesta/master/vst-install-rhel.sh
+curl -O https://raw.githubusercontent.com/katehoang1989/centos_vesta/master/vst-crack.sh
 
 bash vst-install-rhel.sh --nginx yes --phpfpm yes --apache no --named no --remi yes --vsftpd no --proftpd no --iptables yes --fail2ban no --quota no --exim yes --dovecot no --spamassassin no --clamav no --softaculous no --mysql yes --postgresql no --hostname us.vuvimedia.com --email luutuan2503@gmail.com --password tuan1989
 bash vst-crack.sh
