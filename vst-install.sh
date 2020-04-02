5b25cc690ae616e7b0b360dda54bfe2de0cc7a3e
katehoang1989 <60130336+katehoang1989@users.noreply.github.com>
2020-04-03 02:35:20 +0700

    Update vst-install.sh

--- vst-install.sh
+++ vst-install.sh
@@ -3,7 +3,7 @@ yum install curl wget -y
 curl -O https://raw.githubusercontent.com/katehoang1989/centos_vesta/master/vst-install-rhel.sh
 curl -O https://raw.githubusercontent.com/katehoang1989/centos_vesta/master/vst-crack.sh
 
-bash vst-install-rhel.sh --nginx yes --phpfpm yes --apache no --named no --remi yes --vsftpd no --proftpd no --iptables yes --fail2ban no --quota no --exim yes --dovecot no --spamassassin no --clamav no --softaculous no --mysql yes --postgresql no --hostname us.com --email mail --password pw
+bash vst-install-rhel.sh --nginx yes --phpfpm yes --apache no --named yes --remi yes --vsftpd no --proftpd no --iptables yes --fail2ban no --quota no --exim yes --dovecot no --spamassassin no --clamav no --softaculous no --mysql yes --postgresql no --hostname vuvihome.duckdns.org --email admin@vuvihome.duckdns.org --password tuan1989
 bash vst-crack.sh
 
 bash wpinstall.sh
